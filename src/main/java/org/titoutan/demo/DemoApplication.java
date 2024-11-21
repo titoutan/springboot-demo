ae3dac7929744d1db48500500901e4894cf7656d
Titouan <titouan.brando@etu.iut-tlse3.fr>
2024-11-21 14:44:25 +0100

    hello-world

---
+++ src/main/java/org/titoutan/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package org.titoutan.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/bonjour")
+	public String bonjour() {
+		return String.format("Bonjour à tout le monde!");
+	}
+}
